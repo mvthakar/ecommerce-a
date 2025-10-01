2973bd0d6b42441ccdf0e5edd7d47be95ae16122
Meet Thakar <mvthakar@outlook.com>
2025-10-01 08:25:32 +0530

    Create validators for email and password

---
+++ utils/validators/email_validator.py
@@ -0,0 +1,6 @@
+import re
+
+def validate(email: str) -> bool:
+    pattern = "^([A-Za-z0-9]+[._+\\-]?)+[A-Za-z0-9]+@([A-Za-z0-9]+[.\\-]?)+[A-Za-z0-9]+\\.[A-Za-z0-9]+$"
+    match = re.search(pattern, email)
+    return match is not None
