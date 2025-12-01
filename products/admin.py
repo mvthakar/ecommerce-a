e7f0205c659763a908176c5ee048b26082ffdf48
Meet Thakar <mvthakar@outlook.com>
2025-12-01 08:42:25 +0530

    Create admin site for product and category models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductWithFilters(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category',)
+    search_fields = ('name', 'price',)
+    list_filter = ('category',)
+
+admin.site.register(Product, ProductWithFilters)
