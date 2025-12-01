6dbcf0414433f1a31a0547e3a69aff95e0a05f7a
MT <47700487+mvthakar@users.noreply.github.com>
2025-12-01 08:43:38 +0530

    Create admin site for product and category models (#6)

--- categories/models.py
+++ categories/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
   name = models.CharField(max_length=255)
   parent_category = models.ForeignKey("self", null=True, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
